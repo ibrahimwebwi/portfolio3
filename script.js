25122c0f2a096adecbe5f328ec039d89fbc317c2
ibrahimwebwi <bilaljhanzeb9@gmail.com>
2024-06-30 19:10:35 +0500

    added

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+
+
+let menuIcon = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle("bx-x");
+    navlist.classList.toggle("open");
+}
+
+window.onscroll = ()=>{
+    menuIcon.classList.remove("bx-x");
+    navlist.classList.remove("open");
+}
+
+
+
+
+
